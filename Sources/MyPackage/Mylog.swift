9edeee64f41f1a48870af54a3e385439b1eba8ae
QianFeiying <>
2021-02-02 09:56:16 +0800

    22

--- Sources/MyPackage/Mylog.swift
+++ Sources/MyPackage/Mylog.swift
@@ -3,14 +3,13 @@
             File name:     File.swift
             Author:        Qian
             Description:   简介
-            History:      2021/2/1: File created.
+            History:      2021/2/2: File created.
     *******************************************************************/
     
 
 import Foundation
 
-
-class Test {
+class MyLog: NSObject {
     func logTest() {
         print("这是一个测试！！！！")
     }
