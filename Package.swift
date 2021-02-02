5da60671d6a9043c68666fcccb6873c8dadca537
QianFeiying <>
2021-02-02 10:28:56 +0800

    11

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyPackage",
-            dependencies: []),
+            dependencies: ["MyTestLog"]),
         .testTarget(
             name: "MyPackageTests",
             dependencies: ["MyPackage"]),
