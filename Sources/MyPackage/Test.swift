4cfb8ade93563208e30be5bf32c91577ae976032
QianFeiying <>
2021-02-02 09:46:39 +0800

    first

---
+++ Sources/MyPackage/Test.swift
@@ -0,0 +1,17 @@
+//
+    /******************************************************************
+            File name:     File.swift
+            Author:        Qian
+            Description:   简介
+            History:      2021/2/1: File created.
+    *******************************************************************/
+    
+
+import Foundation
+
+
+class Test {
+    func logTest() {
+        print("这是一个测试！！！！")
+    }
+}
